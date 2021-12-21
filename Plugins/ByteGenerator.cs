854dcbbd66525d16f06463b08301e0f555dd3047
darya-cherr <chernikovadarya67@gmail.com>
2021-12-22 01:49:25 +0300

    Create plugins

---
+++ Plugins/ByteGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Faker;
+
+namespace Plugins
+{
+    class ByteGenerator : IValueGenerator
+    {
+        private Random random = new Random();
+
+        public object GenerateValue()
+        {
+            return (byte)(random.Next(byte.MinValue,byte.MaxValue));
+        }
+
+        public Type GetValueType()
+        {
+            return typeof(Byte);
+        }
+    }
+}
---
+++ Plugins/CharGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Faker;
+
+namespace Plugins
+{
+    public class CharGenerator : IValueGenerator
+    {
+        private Random random = new Random();
+        private string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public object GenerateValue()
+        {
+            char ch = chars[random.Next(chars.Length)];
+            return ch;
+        }
+
+        public Type GetValueType()
+        {
+            return typeof(Char);
+        }
+    }
+}
